58117723aa0bcfdad033279cc49b1a0255a3e6af
virgantara <vinux.edu@gmail.com>
2025-09-03 07:11:37 +0700

    update

--- main_cls.py
+++ main_cls.py
@@ -170,26 +170,11 @@ def main(args):
         weights = torch.load(pretrain_path, map_location="cpu", weights_only=False)
         if weights:
             model.load(weights)
-    elif args.model_name == 'yolov8':
-        cfg = os.path.join('yolo/cfg','models','v8',f'yolov8{args.yolo_scale}-cls.yaml')
+    elif args.model_name == 'yolo':
+        cfg = args.path_yolo_yaml
         model = ClassificationModel(cfg, nc=3, ch=3)
         
-        pretrain_path = os.path.join('pretrain','yolov8n-cls.pt')
-        weights = torch.load(pretrain_path, map_location="cpu", weights_only=False)
-        if weights:
-            model.load(weights)
-
-        for m in model.modules():
-            if isinstance(m, torch.nn.Dropout) and args.dropout:
-                m.p = args.dropout  # set dropout
-        for p in model.parameters():
-            p.requires_grad = True  # for training
-    elif args.model_name == 'yolov12':
-        cfg = os.path.join('yolo/cfg','models','12',f'yolo12{args.yolo_scale}-cls.yaml')
-        model = ClassificationModel(cfg, nc=3, ch=3)
-        
-        pretrain_path = os.path.join('pretrain','yolo12n.pt')
-        weights = torch.load(pretrain_path, map_location="cpu", weights_only=False)
+        weights = torch.load(args.pretrain_path, map_location="cpu", weights_only=False)
         if weights:
             model.load(weights)
 
